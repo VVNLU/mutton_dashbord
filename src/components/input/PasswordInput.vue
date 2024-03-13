da5c75bbc36110a4a0fcbebde3a20e80fed3e751
vivian <vivianl@mega7.fun>
2024-03-13 14:55:41 +0800

    fix: 修正程式碼

--- src/components/input/PasswordInput.vue
+++ src/components/input/PasswordInput.vue
@@ -1,3 +1,22 @@
 <template>
-    <base-input :type="'password'" :label="'密碼'" :placeholder="'請輸入密碼'" :validate="true" :errorMessage="'密碼必填。'" />
+    <text-input :label="'密碼'" :placeholder="'請輸入密碼'" v-model="observeValue" :maxLength="15"
+        :type="isPwd ? 'password' : 'text'">
+        <template #append>
+            <q-icon :name="isPwd ? 'visibility_off' : 'visibility'" class="cursor-point" @click="isPwd = !isPwd" />
+        </template>
+    </text-input>
 </template>
+
+<script setup>
+import { ref } from 'vue'
+import { useVModel } from '@vueuse/core';
+
+const props = defineProps({
+    modelValue: { type: [String, Number] }
+})
+const emit = defineEmits(['update:modelValue'])
+
+const observeValue = useVModel(props, 'modelValue', emit)
+const isPwd = ref(true)
+
+</script>
