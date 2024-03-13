58b10ba45d28a11eb49d42bc6c2ffdf264c71020
vivian <vivianl@mega7.fun>
2024-03-13 15:39:12 +0800

    fix: 修正程式碼

--- src/components/button/ConfirmButton.vue
+++ src/components/button/ConfirmButton.vue
@@ -1,3 +1,15 @@
 <template>
-    <base-button :label="'確認'" />
+    <base-button :label="label" :outline="outline" :rounded="rounded" :color="color" />
 </template>
+
+<script setup>
+import { computed, defineProps } from 'vue'
+
+const props = defineProps({
+    label: { type: String },
+    outline: { type: Boolean, default: false },
+    rounded: { type: Boolean, default: false },
+    color: { type: String, default: 'primary' },
+})
+
+</script>
