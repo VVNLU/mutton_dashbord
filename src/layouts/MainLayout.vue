8f3224e281743b7aafbfb2b7e2c9d0b82630cb49
vivian <vivianl@mega7.fun>
2024-03-14 19:33:56 +0800

    add: MainLayout page

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,10 @@
+<template>
+    <q-layout view="lHh Lpr lFf">
+        <Header />
+        <router-view />
+    </q-layout>
+</template>
+
+<script setup>
+import Header from './components/Header.vue'
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,23 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+import MainLayout from '@/layouts/MainLayout.vue'
+
 const constantRoutes = [
   {
     path: '/login',
     component: () => import('@/pages/login/index.vue'),
+  },
+  {
+    path: '/',
+    component: MainLayout,
+    redirect: '/dashboard',
+    children: [
+      {
+        path: 'dashboard',
+        component: () => import('@/pages/dashboard/index.vue'),
+        name: 'Dashboard',
+      },
+    ],
   },
 ]
 
