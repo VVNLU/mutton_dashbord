0089bab649944fe6e19a684239d25b4c962a010e
vivian <vivianl@mega7.fun>
2024-03-14 16:15:31 +0800

    fix: 修正正確寫法

--- src/components/input/BaseForm.vue
+++ src/components/input/BaseForm.vue
@@ -6,31 +6,49 @@
     </q-form>
 </template>
 
-<script setup>
-import { ref, defineEmits } from 'vue';
+<script>
+import { defineComponent, ref } from 'vue-demi'
 
-const emit = defineEmits(['submit', 'reset'])
-const form = ref()
+export default defineComponent({
+    props: {},
+    emits: ['submit', 'reset'],
+    setup(props, { emit }) {
+        // data
+        const form = ref()
 
-const validate = () => {
-    return form.value.validate()
-}
-const resetValidation = () => {
-    return form.value.resetValidation()
-}
-const submit = (evt = null) => {
-    return form.value.submit(evt)
-}
-const reset = (evt = null) => {
-    return form.value.reset(evt)
-}
-const getValidationComponents = () => {
-    return form.value.getValidationComponents()
-}
-const onSubmit = () => {
-    emit('submit')
-}
-const onReset = () => {
-    emit('reset')
-}
+        // methods
+        const validate = () => {
+            return form.value.validate()
+        }
+        const resetValidation = () => {
+            form.value.resetValidation()
+        }
+        const submit = (evt = null) => {
+            form.value.submit(evt)
+        }
+        const reset = (evt = null) => {
+            form.value.reset(evt)
+        }
+        const getValidationComponents = () => {
+            return form.value.getValidationComponents()
+        }
+        const onSubmit = () => {
+            emit('submit')
+        }
+        const onReset = () => {
+            emit('reset')
+        }
+
+        return {
+            form,
+            validate,
+            resetValidation,
+            submit,
+            reset,
+            getValidationComponents,
+            onSubmit,
+            onReset,
+        }
+    },
+})
 </script>
