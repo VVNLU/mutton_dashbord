e659542c1f4482a46905f1f5053dcf6a67481b58
vivian <vivianl@mega7.fun>
2024-03-14 19:34:08 +0800

    add: Header page

---
+++ src/layouts/components/Header.vue
@@ -0,0 +1,9 @@
+<template>
+    <q-header elevated class="bg-grey  text-blue-grey7">
+        <q-toolbar>
+            <q-btn flat dense round icon="menu" aria-label="Menu" />
+            <q-space />
+            <q-btn flat dense round icon="logout" />
+        </q-toolbar>
+    </q-header>
+</template>
