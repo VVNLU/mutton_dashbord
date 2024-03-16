7c17fa876bb16ec1e29e67de3d0a745194194fac
vivian <vivianl@mega7.fun>
2024-03-16 12:19:28 +0800

    add: sidebar

--- src/stores/app.js
+++ src/stores/app.js
@@ -3,10 +3,24 @@ import { defineStore } from 'pinia'
 export const useApp = defineStore({
   id: 'app',
   state: () => ({
+    sidebar: {
+      opened: false,
+      withoutAnimation: false,
+    },
     isLoading: false,
     isCreate: false,
     isUpdate: false,
     isDelete: false,
   }),
-  actions: {},
+  actions: {
+    toggleSidebar() {
+      this.sidebar.opened = !this.sidebar.opened
+      this.sidebar.withoutAnimation = false
+      if (this.sidebar.opened) {
+        localStorage.setItem('sidebarStatus', 1)
+      } else {
+        localStorage.setItem('sidebarStatus', 0)
+      }
+    },
+  },
 })
