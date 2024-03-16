6b35b5835a55b622960f3d177d1fed5b46d4e8de
vivian <vivianl@mega7.fun>
2024-03-16 17:41:37 +0800

    fix: 添加開合 sidebar, 登出邏輯

--- src/layouts/components/Header.vue
+++ src/layouts/components/Header.vue
@@ -1,9 +1,26 @@
 <template>
     <q-header elevated class="bg-grey  text-blue-grey7">
         <q-toolbar>
-            <q-btn flat dense round icon="menu" aria-label="Menu" />
+            <q-btn flat dense round icon="menu" aria-label="Menu" @click="toggle" />
             <q-space />
-            <q-btn flat dense round icon="logout" />
+            <q-btn flat dense round icon="logout" @click="logout" />
         </q-toolbar>
     </q-header>
 </template>
+
+<script setup>
+import { useApp } from '@/stores/app'
+import useLogout from '@/hooks/useLogout.js'
+
+const storeApp = useApp()
+
+const toggle = () => {
+    storeApp.toggleSidebar()
+}
+
+const logout = () => {
+    resetStore()
+}
+
+const { resetStore } = useLogout()
+</script>
