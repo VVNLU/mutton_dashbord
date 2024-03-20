a4e8433830ae60a25c8b131813778d5caf086f4e
vivian <vivianl@mega7.fun>
2024-03-20 14:23:47 +0800

    fix: 將scroll, store.permission隱藏，有用到再打開

--- src/layouts/components/sidebar/Sidebar.vue
+++ src/layouts/components/sidebar/Sidebar.vue
@@ -1,13 +1,14 @@
 <template>
     <q-drawer v-model="sidebarOpened" show-if-above bordered :width="260" class="bg-grey-3 shadow-7 text-blue-grey-7">
         <q-item-label header class="text-center text-h6 header-title">羊肉盧後台管理</q-item-label>
-        <q-scroll-area class="h-[calc(95%_-_50px)]" style="border-right: 1px solid #ddd;">
-            <q-list>
-                <sidebar-item v-for="(routeItem, routeIndex) in routes" :key="routeIndex" :item="routeItem"
-                    :base-path="routeItem.path" />
-            </q-list>
-        </q-scroll-area>
         <q-separator />
+
+        <!--<q-scroll-area class="h-[calc(95%_-_50px)]" style="border-right: 1px solid #ddd;"> -->
+        <q-list>
+            <sidebar-item v-for="(routeItem, routeIndex) in asyncRoutes" :key="routeIndex" :item="routeItem"
+                :base-path="routeItem.path" />
+        </q-list>
+        <!-- </q-scroll-area> -->
     </q-drawer>
 </template>
 
@@ -15,10 +16,11 @@
 import SidebarItem from './SidebarItem.vue'
 import { computed } from 'vue'
 import { useApp } from '@/stores/app'
-import { usePermission } from '@/stores/permission'
+import { asyncRoutes } from '@/router'
+// import { usePermission } from '@/stores/permission'
 
 const store = useApp()
-const storePermission = usePermission()
+// const storePermission = usePermission()
 const sidebarOpened = computed({
     get() {
         return store.sidebar.opened
@@ -28,8 +30,8 @@ const sidebarOpened = computed({
     },
 })
 
-const routes = computed(() => {
-    return storePermission.routes
-})
+// const routes = computed(() => {
+//     return storePermission.routes
+// })
 
 </script>
