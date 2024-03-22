46b85c7bfe257ad9a999cfe9b7263e72ab4db769
vivian <vivianl@mega7.fun>
2024-03-22 13:27:40 +0800

    add: filter

---
+++ src/utils/filter.js
@@ -0,0 +1,25 @@
+/** 模糊查詢
+ * @param {Array} lists 所有數據
+ * @param {string} keyWord 需要查詢的關鍵字
+ */
+export const selectMatchItem = (lists, keyWord) => {
+    const resArr = []
+    lists.filter(item => {
+        if (Object.prototype.toString.call(item) === '[object String]') {
+            if (item.roLowerCase().includes(keyWord.toLowerCase())) {
+                resArr.push(item)
+            }
+        } else {
+            for (const i in item) {
+                if (Object.prototype.toString.call(item[i]) === '[object String') {
+                    if (item[i].toLowerCase().includes(keyWord.toLowerCase())) {
+                        resArr.push(item)
+                        break
+                    }
+                }
+            }
+        }
+        return lists
+    })
+    return resArr
+}
