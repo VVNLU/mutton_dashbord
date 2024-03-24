23145bafedd63ed02e25c88d1ce723d35dc8b016
vivian <vivianl@mega7.fun>
2024-03-24 10:04:40 +0800

    add: ToggleInput component

--- src/components/index.js
+++ src/components/index.js
@@ -2,6 +2,7 @@ import BaseForm from './input/BaseForm.vue'
 import TextInput from './input/TextInput.vue'
 import PasswordInput from './input/PasswordInput.vue'
 import SelectInput from './input/SelectInput.vue'
+import ToggleInput from './input/ToggleInput.vue'
 import BaseButton from './button/BaseButton.vue'
 import ConfirmButton from './button/ConfirmButton.vue'
 import CancelButton from './button/CancelButton.vue'
@@ -17,6 +18,7 @@ export default {
     app.component('TextInput', TextInput)
     app.component('PasswordInput', PasswordInput)
     app.component('SelectInput', SelectInput)
+    app.component('ToggleInput', ToggleInput)
     app.component('BaseButton', BaseButton)
     app.component('ConfirmButton', ConfirmButton)
     app.component('CancelButton', CancelButton)
---
+++ src/components/input/ToggleInput.vue
@@ -0,0 +1,16 @@
+<template>
+    <q-toggle v-model="observeVale" :left-label="leftLabel" />
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+import { useVModel } from '@vueuse/core'
+
+const props = defineProps({
+    modelValue: { type: Boolean, default: true },
+    leftLabel: :{ type: Boolean, default: true }
+})
+const emit = (['update:modelValue'])
+
+const observeVale = useVModel(props, 'modelValue', emit)
+</script>
