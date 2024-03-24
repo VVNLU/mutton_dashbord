604d37bf3f3d40ac59b50ece4d23b3c8651e31ac
vivian <vivianl@mega7.fun>
2024-03-24 11:49:40 +0800

    fix: 將 label直接使用，不使用computed

--- src/components/button/DeleteIconButton.vue
+++ src/components/button/DeleteIconButton.vue
@@ -1,18 +1,12 @@
 <template>
-    <base-icon-button color="red" icon="delete" :outline="outline" :label="buttonLabel" />
+    <base-icon-button color="red" icon="delete" :outline="outline" :label="label" />
 </template>
 
 <script setup>
-import { defineProps, toRefs, computed } from 'vue';
+import { defineProps } from 'vue';
 
 const props = defineProps({
     label: { type: String },
     outline: { type: Boolean, default: true }
-})
-
-const { label } = toRefs(props)
-
-const buttonLabel = computed(() => {
-    return label.value
 })
 </script>
