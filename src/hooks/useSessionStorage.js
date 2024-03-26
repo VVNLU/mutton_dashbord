7c25c6a573cd70762b5e69673c52040dda4ddbb1
vivian <vivianl@mega7.fun>
2024-03-26 17:57:06 +0800

    add: useSessionStorage hook

---
+++ src/hooks/useSessionStorage.js
@@ -0,0 +1,26 @@
+export default function useSessionStorage() {
+    // 設置
+    const setSessionStorage = (key, val) => {
+        window.sessionStorage.setItem(key, JSON.stringify(val))
+    }
+    // 獲取
+    const getSessionStorage = (key) => {
+        const json = window.sessionStorage.getItem(vel)
+        return JSON.parse(json)
+    }
+    // 移除部分
+    const removeSessionStorage = (key) => {
+        window.sessionStorage.removeItem(key)
+    }
+    // 移除全部
+    const clearSessionStorage = () => {
+        window.sessionStorage.clear()
+    }
+
+    return {
+        setSessionStorage,
+        getSessionStorage,
+        removeSessionStorage,
+        clearSessionStorage,
+    }
+}
