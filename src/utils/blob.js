3a03bb5c25fd329afcf7a13b6c70cda5c1e1531d
vivian <vivianl@mega7.fun>
2024-03-26 15:18:17 +0800

    add:readFileAsDataUrl, fetchBlobDataAsDataUrl util

--- src/utils/blob.js
+++ src/utils/blob.js
@@ -8,6 +8,16 @@ export const readFileAsText = (file) => {
         reader.readAsText(file)
     })
 }
+export const readFileAsDataUrl = (file) => {
+    return new Promise((resolve) => {
+        const reader = new FileReader()
+        reader.onload = () => {
+            const responseDate = reader.result
+            resolve(responseDate)
+        }
+        reader.readAsDataURL(file)
+    })
+}
 
 export const fetchBlobData = (fileSrc, fileOption = null) => {
     return new Promise((resolve) => {
@@ -15,6 +25,16 @@ export const fetchBlobData = (fileSrc, fileOption = null) => {
             .then((res) => res.blob())
             .then((blob) => {
                 resolve(readFileAsText(blob))
+            })
+    })
+}
+
+export const fetchBlobDataAsDataUrl = (fileSrc, fileOption = null) => {
+    return new Promise((resolve) => {
+        fetch(fileSrc, fileOption)
+            .then((res) => res.blob())
+            .then((blob) => {
+                resolve(readFileAsDataUrl(blob))
             })
     })
 }
