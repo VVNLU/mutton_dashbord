c0c22b2083dc3c45dacfd1681746311bcacad62f
vivian <vivianl@mega7.fun>
2024-03-26 17:04:46 +0800

    add: useMessageDialog hook

---
+++ src/hooks/useMessageDialog.js
@@ -0,0 +1,42 @@
+import { useMessageDialogStore } from '@/stores/messageDialog'
+
+export default function useMessageDialog() {
+    const store = useMessageDialogStore()
+
+    const messageAlert = ({ title, message, confirmButtonText, confirmButtonColor }) => store.alert({
+        title,
+        message,
+        persistent: false,
+        confirmButtonText,
+        confirmButtonColor: confirmButtonColor || 'primary'
+    })
+
+    const messageConfirm = ({ title, message, confirmButtonText, confirmButtonColor, cancelButtonText, cancelButtonColor }) => store.alert({
+        title,
+        message,
+        persistent: true,
+        confirmButtonText,
+        confirmButtonColor: confirmButtonColor || 'primary',
+        showCancel: true,
+        cancelButtonText,
+        cancelButtonColor: cancelButtonColor || 'primary'
+    })
+
+    const messageCancel = ({ title, message, cancelButtonText, cancelButtonColor }) => store.alert({
+        title,
+        status: 'error',
+        message,
+        persistent: true,
+        confirmButtonText,
+        confirmButtonColor: 'negative',
+        showCancel: true,
+        cancelButtonText,
+        cancelButtonColor: cancelButtonColor || 'primary'
+    })
+
+    return {
+        messageAlert,
+        messageConfirm,
+        messageCancel
+    }
+}
