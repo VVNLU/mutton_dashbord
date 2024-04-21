de24dfa5fad0fb16f7efe3cd026e27766a9cfd83
vivian <vivianl@mega7.fun>
2024-04-21 11:50:26 +0800

    fix: 添加.env

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -6,12 +6,12 @@ import 'firebase/storage'
 import 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDnKjgmrVSFRb63LeBDd6YMDlyuz5JSkzM",
-  authDomain: "mutton-dashboard.firebaseapp.com",
-  projectId: "mutton-dashboard",
-  storageBucket: "mutton-dashboard.appspot.com",
-  messagingSenderId: "820459566052",
-  appId: "1:820459566052:web:275d20c8354e945b9a4df1"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
 }
 
 const app = firebase.initializeApp(firebaseConfig) // 初始化
