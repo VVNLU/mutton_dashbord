59765a311c398b25c8995ccdcfa413213940aee6
vivian <vivianl@mega7.fun>
2024-04-23 14:16:29 +0800

    fix: 修正錯誤寫法

--- src/hooks/useQuickState.js
+++ src/hooks/useQuickState.js
@@ -1,21 +1,21 @@
 import { reactive } from "vue";
-import { cloneDeep } from "lodash/cloneDeep";
+import cloneDeep from "lodash/cloneDeep";
 
 export default function useQuickState(params = {}) {
-    const state = reactive({ ...params })
-    const backupState = cloneDeep(state)
-    const reset = () => {
-        Object.assign(state, cloneBackup())
-    }
-    const clone = cloneDeep(state)
-    const cloneBackup = () => cloneDeep(backupState)
+  const state = reactive({ ...params })
+  const backupState = cloneDeep(state)
+  const reset = () => {
+    Object.assign(state, cloneBackup())
+  }
+  const clone = cloneDeep(state)
+  const cloneBackup = () => cloneDeep(backupState)
 
-    return {
-        state,
-        backupState,
-        clone,
-        reset,
-        assign: (newParams) = Object.assign(state, newParams),
-        cloneBackup
-    }
+  return {
+    state,
+    backupState,
+    clone,
+    reset,
+    assign: (newParams) => Object.assign(state, newParams),
+    cloneBackup
+  }
 }
