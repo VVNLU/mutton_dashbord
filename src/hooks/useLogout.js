1689c13dd08d9d58a0d18ffe7ce7537237b42b1b
vivian <vivianl@mega7.fun>
2024-04-24 16:27:06 +0800

    fix: 添加登出 API

--- src/hooks/useLogout.js
+++ src/hooks/useLogout.js
@@ -1,10 +1,13 @@
+import { useUser } from '@/stores/user'
 import router from '@/router'
 import useNotify from './useNotify'
 
 export default function useLogout() {
   const { notify } = useNotify()
+  const stores = useUser()
 
-  const resetStore = () => {
+  const resetStore = async () => {
+    await stores.logout()
     notify({ message: '登出成功', type: 'positive' })
     router.push('/login')
   }
