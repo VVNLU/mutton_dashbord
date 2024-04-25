23ac48f558dd32a7f419d0f10d4331ccda2cdf8d
vivian <vivianl@mega7.fun>
2024-04-25 15:59:04 +0800

    fix: 改寫正確格式

--- src/hooks/useImgStorage.js
+++ src/hooks/useImgStorage.js
@@ -1,32 +1,26 @@
-import { ref, onUnmounted } from 'vue';
-import { getStorage, ref as storageRef, getDownloadURL } from 'firebase/storage';
-import { app as firebaseApp } from '@/firebase/index';
+import { db } from '@/firebase'
 
 export default function useImgStorage() {
-    const storage = getStorage(firebaseApp);
-    const fileBaseUrl = ref('');
+  const getImageSrc = async ({ filename, size = '100x' }) => {
+    try {
+      if (!filename) return ''
 
-    const fetchFileBaseUrl = async () => {
-        try {
-            const bucketUrl = 'gs://mutton-dashboard.appspot.com';
-            const url = await getDownloadURL(storageRef(storage, bucketUrl));
-            fileBaseUrl.value = url;
-        } catch (error) {
-            console.error('Failed to fetch file base URL:', error);
-        }
-    };
-
-    fetchFileBaseUrl();
-
-    onUnmounted(() => {
-        fileBaseUrl.value = '';
-    });
-
-    const getImageSrc = ({ filename, size = '100x' }) => {
-        if (!filename) return '';
-        return `${fileBaseUrl.value}/display/resize/${size}/${filename}`;
-    };
-    return {
-        getImageSrc
-    };
+      const docRef = db.collection('config').doc('fileServerHost')
+      const docSnapshot = await docRef.get()
+      if (docSnapshot.exists()) {
+        const fileServerHost = docSnapshot.data().value
+        const url = `${fileServerHost}/display/resize/${size}/${filename}`
+        return url
+      } else {
+        console.log('File server host configuration not found in Firebase')
+        return ''
+      }
+    } catch (error) {
+      console.log('Error fetching file server hot configuration from Firebase')
+      return ''
+    }
+  }
+  return {
+    getImageSrc
+  }
 }
