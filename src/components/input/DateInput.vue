dde383acdb85c5df983078a810e842dd4f798df1
vivian <vivianl@mega7.fun>
2024-04-26 11:25:47 +0800

    fix: 修正錯誤的寫法

--- src/components/input/DateInput.vue
+++ src/components/input/DateInput.vue
@@ -1,23 +1,23 @@
 <template>
-    <text-input ref="inputData" v-model="observeValue" :label="label" :placeholder="placeholder" :rules="ruleList"
-        inputmode="none">
-        <template #append>
-            <q-icon name="event" />
-        </template>
-        <template v-if="$slots.hint" #hint>
-            <slot name="hint" />
-        </template>
-        <template #default>
-            <q-popup-proxy v-model="show" class="flex justify-center" transition-show="scale" transition-hide="scale">
-                <q-date v-model="observeValue" :title="dateTitle" :subtitle="dateSubtitle" :options="observeOptions"
-                    @update:model="updateModelValue">
-                    <div class="row items-center justify-end">
-                        <q-btn v-close-popup label="Close" color="primary" flat />
-                    </div>
-                </q-date>
-            </q-popup-proxy>
-        </template>
-    </text-input>
+  <text-input ref="inputData" v-model="observeValue" :label="label" :placeholder="placeholder" :rules="ruleList"
+    inputmode="none">
+    <template #append>
+      <q-icon name="event" />
+    </template>
+    <template v-if="$slots.hint" #hint>
+      <slot name="hint" />
+    </template>
+    <template #default>
+      <q-popup-proxy v-model="show" class="flex justify-center" transition-show="scale" transition-hide="scale">
+        <q-date v-model="observeValue" :title="dateTitle" :subtitle="dateSubtitle" :options="observeOptions"
+          @update:modelValue="updateModelValue">
+          <div class="row items-center justify-end">
+            <q-btn v-close-popup label="Close" color="primary" flat />
+          </div>
+        </q-date>
+      </q-popup-proxy>
+    </template>
+  </text-input>
 </template>
 
 <script setup>
@@ -26,13 +26,13 @@ import { computed, defineProps, ref, toRefs } from 'vue';
 import { vuelidate } from '@/plugins/vuelidate'
 
 const props = defineProps({
-    modelValue: { type: String },
-    label: { type: String },
-    placeholder: { type: String, default: '年/月/日' },
-    options: { type: Array },
-    min: { type: String },
-    max: { type: String },
-    rules: { type: Array, default() { return [] } }
+  modelValue: { type: String },
+  label: { type: String },
+  placeholder: { type: String, default: '年/月/日' },
+  options: { type: Array },
+  min: { type: String },
+  max: { type: String },
+  rules: { type: Array, default() { return [] } }
 })
 const emit = defineEmits(['update:modelValue'])
 
@@ -42,41 +42,41 @@ const { min, max, options, rules } = toRefs(props)
 const observeValue = useVModel(props, 'update:modelValue', emit)
 
 const dateTitle = computed(() => {
-    return !observeValue ? ' ' : ''
+  return !observeValue.value ? ' ' : ''
 })
 
 const dateSubtitle = computed(() => {
-    return !observeValue ? ' ' : ''
+  return !observeValue.value ? ' ' : ''
 })
 
 const observeOptions = computed(() => {
-    const _options = options.value
-    const dateFn = (date) => {
-        const _min = min.value
-        const _max = max.value
-        if (!_min && _max) return true
-        if (_min && _max) return date >= _min && date <= _max
-        if (_min) return date >= _min
-        if (_max) return date <= _max
-    }
-    if (_options && _options.length > 0) return _options
-    return dateFn
+  const _options = options.value
+  const dateFn = (date) => {
+    const _min = min.value
+    const _max = max.value
+    if (!_min && !_max) return true
+    if (_min && _max) return date >= _min && date <= _max
+    if (_min) return date >= _min
+    if (_max) return date <= _max
+  }
+  if (_options && _options.length > 0) return _options
+  return dateFn
 })
 
 const ruleList = computed(() => {
-    const rule = []
-    min.value && rule.push(vuelidate.dayjIssameOrAfter(min.value, `必須比${min.value}晚`))
-    max.value && rule.push(vuelidate.dayjIssameOrBefore(max.value, `必須比${min.value}早`))
-    return rule.concat(rules.value)
+  const rule = []
+  min.value && rule.push(vuelidate.dayjIsSameOrAfter(min.value, `必需比${min.value}晚`))
+  max.value && rule.push(vuelidate.dayjIsSameOrBefore(max.value, `必需比${max.value}早`))
+  return rule.concat(rules.value)
 })
 
-const showPop = (isShow) => {
-    setTimeout(() => {
-        show.value = isShow
-    }, 200)
+const showPopup = (isShow) => {
+  setTimeout(() => {
+    show.value = isShow
+  }, '200')
 }
 
 const updateModelValue = (value, reason, details) => {
-    if (value) showPop(false)
+  if (value) showPopup(false)
 }
 </script>
