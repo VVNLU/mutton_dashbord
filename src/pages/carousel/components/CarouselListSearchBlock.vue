1252e827347ef1113c151539bd66132be844b727
vivian <vivianl@mega7.fun>
2024-04-27 21:35:45 +0800

    fix:修正縮排、命名錯誤

--- src/pages/carousel/components/CarouselListSearchBlock.vue
+++ src/pages/carousel/components/CarouselListSearchBlock.vue
@@ -1,14 +1,14 @@
 <template>
-    <list-search-block v-model="sourceData" @changeFilter="onChangeFilter" @reset="onReset">
-        <div class="col-md-3 col-sm-6 col-xs-12">
-            <date-range-input v-model="sourceData.publish_date_range" class="w-full" dense
-                placeholder="請選擇上架開始日期至上架結束日期" @update:modelValue="changeFilter" />
-        </div>
-        <div class="col-md-3 col-sm-6 col-xs-12">
-            <date-range-input v-model="sourceData.closed_date_range" class="w-full" dense placeholder="請選擇下架開始日期至下架結束日期"
-                @update:model="onChangeFilter" />
-        </div>
-    </list-search-block>
+  <list-search-block v-model="sourceData" @changeFilter="onChangeFilter" @reset="onReset">
+    <div class="col-md-3 col-sm-6 col-xs-12">
+      <date-range-input v-model="sourceData.publish_date_range" class="w-full" dense placeholder="請選擇上架開始日期至上架結束日期"
+        @update:modelValue="onChangeFilter" />
+    </div>
+    <div class="col-md-3 col-sm-6 col-xs-12">
+      <date-range-input v-model="sourceData.closed_date_range" class="w-full" dense placeholder="請選擇下架開始日期至下架結束日期"
+        @update:modelValue="onChangeFilter" />
+    </div>
+  </list-search-block>
 </template>
 
 <script setup>
@@ -16,18 +16,18 @@ import { useVModel } from '@vueuse/core'
 import { defineProps } from 'vue';
 
 const props = defineProps({
-    modelValue: { type: [Object, String] }
+  modelValue: { type: [Object, String] }
 })
 const emit = defineEmits(['changeFilter', 'reset'])
 
 const sourceData = useVModel(props, 'modelValue', emit)
 
 const onChangeFilter = () => {
-    emit('changeFilter')
+  emit('changeFilter')
 }
 
-const reset = () => {
-    defineEmits('reset')
+const onReset = () => {
+  defineEmits('reset')
 }
 </script>
 
