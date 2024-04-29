24e6932b109a2a5f7a4108ff36362dd640d4939c
vivian <vivianl@mega7.fun>
2024-04-29 17:30:11 +0800

    fix: 縮排調整

--- src/components/ListSearchBlock.vue
+++ src/components/ListSearchBlock.vue
@@ -1,13 +1,13 @@
 <template>
-    <div class="row q-col-gutter-x-md q-col-gutter-y-xs">
-        <slot name="default" />
-        <div v-if="showInputSearch" :class="inputSearchCol">
-            <search-input v-model="sourceData.keyword" class="w-full" dense @update:modelValue="onChangeFilter" />
-        </div>
-        <div v-if="showResetButton" :class="resetButtonCol">
-            <base-button class="w-full" @click="onReset" text-color="black" :label="'清除'" color="white" />
-        </div>
+  <div class="row q-col-gutter-x-md q-col-gutter-y-xs">
+    <slot name="default" />
+    <div v-if="showInputSearch" :class="inputSearchCol">
+      <search-input v-model="sourceData.keyword" class="w-full" dense @update:modelValue="onChangeFilter" />
     </div>
+    <div v-if="showResetButton" :class="resetButtonCol">
+      <base-button class="w-full" @click="onReset" text-color="black" :label="'清除'" color="white" />
+    </div>
+  </div>
 </template>
 
 <script setup>
@@ -15,21 +15,21 @@ import { useVModel } from '@vueuse/core'
 import { defineProps } from 'vue';
 
 const props = defineProps({
-    modelValue: { type: [Object, String] },
-    showInputSearch: { type: Boolean, default: true },
-    showResetButton: { type: Boolean, default: true },
-    inputSearchCol: { type: String, default: 'col-md-3 col-sm-4 col-xs-12' },
-    resetButtonCol: { type: String, default: 'col-lg-1 col-md-3 col-sm-2 col-xs-12' },
+  modelValue: { type: [Object, String] },
+  showInputSearch: { type: Boolean, default: true },
+  showResetButton: { type: Boolean, default: true },
+  inputSearchCol: { type: String, default: 'col-md-3 col-sm-4 col-xs-12' },
+  resetButtonCol: { type: String, default: 'col-lg-1 col-md-3 col-sm-2 col-xs-12' },
 })
 const emit = defineEmits(['reset', 'changeFilter'])
 
 const sourceData = useVModel(props, 'modelValue', emit)
 
 const onChangeFilter = () => {
-    emit('changeFilter')
+  emit('changeFilter')
 }
 
 const onReset = () => {
-    emit('reset')
+  emit('reset')
 }
 </script>
