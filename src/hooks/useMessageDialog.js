75fc6c99c01792e2385c971feda0b55a45e363d6
vivian <vivianl@mega7.fun>
2024-04-29 17:32:02 +0800

    fix: 縮排調整、帶入按鈕文字

--- src/hooks/useMessageDialog.js
+++ src/hooks/useMessageDialog.js
@@ -1,42 +1,43 @@
 import { useMessageDialogStore } from '@/stores/messageDialog'
 
 export default function useMessageDialog() {
-    const store = useMessageDialogStore()
+  const store = useMessageDialogStore()
 
-    const messageAlert = ({ title, message, confirmButtonText, confirmButtonColor }) => store.alert({
-        title,
-        message,
-        persistent: false,
-        confirmButtonText,
-        confirmButtonColor: confirmButtonColor || 'primary'
-    })
+  const messageAlert = ({ title, message, confirmButtonText, confirmButtonColor }) => store.alert({
+    title: title,
+    message,
+    persistent: false,
+    showCancel: false,
+    confirmButtonText: confirmButtonText,
+    confirmButtonColor: confirmButtonColor || 'primary',
+  })
 
-    const messageConfirm = ({ title, message, confirmButtonText, confirmButtonColor, cancelButtonText, cancelButtonColor }) => store.alert({
-        title,
-        message,
-        persistent: true,
-        confirmButtonText,
-        confirmButtonColor: confirmButtonColor || 'primary',
-        showCancel: true,
-        cancelButtonText,
-        cancelButtonColor: cancelButtonColor || 'primary'
-    })
+  const messageConfirm = ({ title, message, confirmButtonText, confirmButtonColor, cancelButtonText, cancelButtonColor }) => store.alert({
+    title: title,
+    message,
+    persistent: true,
+    confirmButtonText: confirmButtonText,
+    confirmButtonColor: confirmButtonColor || 'primary',
+    showCancel: true,
+    cancelButtonText: cancelButtonText,
+    cancelButtonColor: cancelButtonColor || 'primary',
+  })
 
-    const messageDelete = ({ title, message, cancelButtonText, cancelButtonColor }) => store.alert({
-        title,
-        status: 'error',
-        message,
-        persistent: true,
-        confirmButtonText,
-        confirmButtonColor: 'negative',
-        showCancel: true,
-        cancelButtonText,
-        cancelButtonColor: cancelButtonColor || 'primary'
-    })
+  const messageDelete = ({ title, message, confirmButtonText, cancelButtonText, cancelButtonColor }) => store.alert({
+    title: title,
+    status: 'error',
+    message,
+    persistent: true,
+    confirmButtonText: confirmButtonText,
+    confirmButtonColor: 'negative',
+    showCancel: true,
+    cancelButtonText: cancelButtonText,
+    cancelButtonColor: cancelButtonColor || 'primary',
+  })
 
-    return {
-        messageAlert,
-        messageConfirm,
-        messageDelete
-    }
+  return {
+    messageAlert,
+    messageConfirm,
+    messageDelete
+  }
 }
