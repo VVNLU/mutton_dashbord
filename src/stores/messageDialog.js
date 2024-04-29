7fe8c3fcaf29d7b7a05ceab4a0c31dc9c5c56ce6
vivian <vivianl@mega7.fun>
2024-04-29 17:50:03 +0800

    fix: 縮排調整、修正 actions 正確名稱

--- src/stores/messageDialog.js
+++ src/stores/messageDialog.js
@@ -1,52 +1,52 @@
 import { defineStore } from "pinia";
 
 export const useMessageDialogStore = defineStore({
-    id: 'messageDialog',
-    state: () => ({
-        show: false,
-        title: '',
-        message: '',
-        persistent: false,
-        confirmButtonText: '',
-        confirmButtonColor: 'primary',
-        cancelButtonText: '',
-        cancelButtonColor: 'primary',
-        showCancel: true,
-        resolve: null
-    }),
-    action: {
-        alert({
-            title,
-            message,
-            persistent,
-            confirmButtonText,
-            confirmButtonColor,
-            cancelButtonText,
-            cancelButtonColor,
-            showCancel
-        }) {
-            this.title = title
-            this.message = message
-            this.persistent = persistent || this.persistent
-            this.confirmButtonText = confirmButtonText
-            this.confirmButtonColor = confirmButtonColor
-            this.cancelButtonText = cancelButtonText
-            this.cancelButtonColor = cancelButtonColor
-            this.showCancel = showCancel
-            this.show = true
-            return new Promise(resolve => {
-                this.resolve = resolve
-            })
-        },
-        confirm() {
-            if (this.resolve) {
-                this.resolve(true)
-            }
-            this.close()
-        },
-        close() {
-            this.show = false
-            this.$reset()
-        },
+  id: 'messageDialog',
+  state: () => ({
+    show: false,
+    title: '',
+    message: '',
+    persistent: false,
+    confirmButtonText: '',
+    confirmButtonColor: 'primary',
+    cancelButtonText: '',
+    cancelButtonColor: 'primary',
+    showCancel: true,
+    resolve: null
+  }),
+  actions: {
+    alert({
+      title,
+      message,
+      persistent,
+      confirmButtonText,
+      confirmButtonColor,
+      cancelButtonText,
+      cancelButtonColor,
+      showCancel
+    }) {
+      this.title = title
+      this.message = message
+      this.persistent = persistent || this.persistent
+      this.confirmButtonText = confirmButtonText
+      this.confirmButtonColor = confirmButtonColor
+      this.cancelButtonText = cancelButtonText
+      this.cancelButtonColor = cancelButtonColor
+      this.showCancel = showCancel
+      this.show = true
+      return new Promise(resolve => {
+        this.resolve = resolve
+      })
     },
+    confirm() {
+      if (this.resolve) {
+        this.resolve(true)
+      }
+      this.close()
+    },
+    close() {
+      this.show = false
+      this.$reset()
+    },
+  },
 })
