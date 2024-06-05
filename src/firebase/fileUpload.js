873b16e5de729dae43055631e94ff6554984cb8f
vivian <vivianl@mega7.fun>
2024-06-05 15:18:53 +0800

    feat: 包裝 imageObj 並另存在 Storage 中

---
+++ src/firebase/fileUpload.js
@@ -0,0 +1,20 @@
+import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
+import { storage } from "./index"
+
+/**
+ * 上傳文件到 Firebase Storage 並返回文件的下載 URL
+ * @param {File} file - 要上傳的文件
+ * @returns {Promise<string>} - 文件的下載 URL
+ */
+
+export const uploadFile = async (file) => {
+  if (!file) {
+    throw new Error("No file provided for upload.")
+  }
+
+  const storageRef = ref(storage, `images/${file.name}`)
+  await uploadBytes(storageRef, file) // 上傳文件
+  const downloadURL = await getDownloadURL(storageRef) // 獲取文件下載的 URL
+
+  return downloadURL
+}
