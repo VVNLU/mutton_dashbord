67aa1975c811f0c6bfe79438320b3d62f2f5380f
vivian <vivianl@mega7.fun>
2024-06-11 19:43:08 +0800

    fix: 添加字體陰影

--- src/layouts/components/sidebar/SidebarLink.vue
+++ src/layouts/components/sidebar/SidebarLink.vue
@@ -1,10 +1,10 @@
 <template>
     <q-item v-ripple :to="to" clickable class="text-white">
         <q-item-section avatar>
-            <q-icon :name="icon" size="1.8rem" />
+            <q-icon class="sidebar-icon" :name="icon" size="1.8rem" />
         </q-item-section>
         <q-item-section>
-            <q-item-label class="text-bold text-subtitle1">{{ title }}</q-item-label>
+            <q-item-label class="sidebar-label text-bold text-subtitle1">{{ title }}</q-item-label>
         </q-item-section>
     </q-item>
 </template>
@@ -18,3 +18,10 @@ const props = defineProps({
     title: { type: String, default: '' }
 })
 </script>
+
+<style scoped>
+.sidebar-label,
+.sidebar-icon {
+    text-shadow: 2px 2px 1px rgba(0, 0, 0, 0.5);
+}
+</style>
