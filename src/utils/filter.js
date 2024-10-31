cd29013d37be332f351abb27d32cece089a93e4e
Alvis Lu <uualvislu@gmail.com>
2024-10-31 17:31:42 +0800

    fix: fmt

--- src/utils/filter.js
+++ src/utils/filter.js
@@ -3,23 +3,23 @@
  * @param {string} keyWord 需要查詢的關鍵字
  */
 export const selectMatchItem = (lists, keyWord) => {
-    const resArr = []
-    lists.filter(item => {
-        if (Object.prototype.toString.call(item) === '[object String]') {
-            if (item.roLowerCase().includes(keyWord.toLowerCase())) {
-                resArr.push(item)
-            }
-        } else {
-            for (const i in item) {
-                if (Object.prototype.toString.call(item[i]) === '[object String') {
-                    if (item[i].toLowerCase().includes(keyWord.toLowerCase())) {
-                        resArr.push(item)
-                        break
-                    }
-                }
-            }
+  const resArr = []
+  lists.filter((item) => {
+    if (Object.prototype.toString.call(item) === '[object String]') {
+      if (item.roLowerCase().includes(keyWord.toLowerCase())) {
+        resArr.push(item)
+      }
+    } else {
+      for (const i in item) {
+        if (Object.prototype.toString.call(item[i]) === '[object String') {
+          if (item[i].toLowerCase().includes(keyWord.toLowerCase())) {
+            resArr.push(item)
+            break
+          }
         }
-        return lists
-    })
-    return resArr
+      }
+    }
+    return lists
+  })
+  return resArr
 }
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,18 +1,18 @@
 export const getValueByPath = function (object, prop) {
-    if (typeof object !== 'object') return object
-    prop = prop || ''
-    const paths = prop.split('.')
-    let current = object
-    let result = null
-    for (let i = 0, j = paths.length; i < j; i++) {
-        const path = paths[i]
-        if (!current) break
+  if (typeof object !== 'object') return object
+  prop = prop || ''
+  const paths = prop.split('.')
+  let current = object
+  let result = null
+  for (let i = 0, j = paths.length; i < j; i++) {
+    const path = paths[i]
+    if (!current) break
 
-        if (i === j - 1) {
-            result = current[path]
-            break
-        }
-        current = current[path]
+    if (i === j - 1) {
+      result = current[path]
+      break
     }
-    return result
+    current = current[path]
+  }
+  return result
 }
