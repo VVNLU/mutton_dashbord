b8723174f6b16c0628e359aa50ae4de7bd99f5db
Alvis Lu <uualvislu@gmail.com>
2024-10-31 13:04:36 +0800

    fix: lint

---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+// eslint.config.js
+import js from '@eslint/js';
+import vue from 'eslint-plugin-vue';
+import prettier from 'eslint-plugin-prettier';
+
+export default [
+  {
+    files: ['**/*.js', '**/*.vue'],
+    ignores: ['node_modules', 'dist'],
+  },
+  js.configs.recommended,
+  {
+    plugins: {
+      vue,
+      prettier,
+    },
+    rules: {
+      ...vue.configs['vue3-recommended'].rules,
+      'prettier/prettier': 'error', // Prettier 規則
+    },
+  },
+];
