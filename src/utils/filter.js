4abd1cd0a79fa1ed7ddd5dc934dc92e609954ed0
vivian <vivianl@mega7.fun>
2024-11-11 15:47:22 +0800

    fix: 修正錯字

--- src/utils/filter.js
+++ src/utils/filter.js
@@ -6,12 +6,12 @@ export const selectMatchItem = (lists, keyWord) => {
   const resArr = []
   lists.filter((item) => {
     if (Object.prototype.toString.call(item) === '[object String]') {
-      if (item.roLowerCase().includes(keyWord.toLowerCase())) {
+      if (item.toLowerCase().includes(keyWord.toLowerCase())) {
         resArr.push(item)
       }
     } else {
       for (const i in item) {
-        if (Object.prototype.toString.call(item[i]) === '[object String') {
+        if (Object.prototype.toString.call(item[i]) === '[object String]') {
           if (item[i].toLowerCase().includes(keyWord.toLowerCase())) {
             resArr.push(item)
             break
