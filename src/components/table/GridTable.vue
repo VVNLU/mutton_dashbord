0ac9ba6c823fcaa64b6f802f3477fb54a14a9e18
vivian <vivianl@mega7.fun>
2024-11-26 20:36:26 +0800

    fix: 重新命名為 GridTable

--- src/components/table/GridTable.vue
+++ src/components/table/GridTable.vue
@@ -7,21 +7,22 @@
       </span>
     </div>
     <q-card flat bordered v-for="(row, index) in rows" :key="index">
-      <q-card-section class="row items-start q-gutter-md">
+      <q-card-section class="row items-start q-gutter-md ">
         <div v-for="column in columns" :key="column.name">
           <div class="text-bold text-16px q-pb-xs">
             {{ column.label }}
           </div>
-          <div>
-            <span v-if="column.name !== 'quantity' && column.name !== 'price'" class=" rowStyle">
+          <div class="row flex-center">
+            <span v-if="column.name !== 'quantity' && column.name !== 'price' && column.name !== 'total'"
+              class=" rowStyle">
               {{ row[column.name] }}
             </span>
             <span v-else-if="column.name === 'price'" class="rowStyle">
               $ {{ row[column.name] }}
             </span>
-            <div v-else style="width: 80px">
+            <span v-else style="width: 80px">
               <q-input type="number" v-model.number="row[column.name]" dense autofocus clearable />
-            </div>
+            </span>
           </div>
         </div>
         <q-space />
@@ -29,21 +30,6 @@
           <slot name="action" :row="row" />
         </q-card-actions>
       </q-card-section>
-
-      <!-- <下拉選單 /> -->
-      <q-card-actions v-show="isExpanded">
-        <!-- <q-space /> -->
-        <q-btn color="grey" round flat dense :icon="expanded ? 'keyboard_arrow_up' : 'keyboard_arrow_down'"
-          @click="expanded = !expanded" />
-      </q-card-actions>
-      <q-slide-transition>
-        <div v-show="expanded">
-          <q-separator />
-          <q-card-section>
-            <p>123</p>
-          </q-card-section>
-        </div>
-      </q-slide-transition>
     </q-card>
   </div>
 </template>
@@ -52,19 +38,16 @@
 import { defineProps, ref } from 'vue'
 
 const props = defineProps({
-  isExpanded: { type: Boolean, default: true },
   columns: { type: Array, default: () => [] },
   rows: { type: Array, default: () => [] },
 })
-
-const expanded = ref(false)
 </script>
 
 <style lang="scss" scoped>
 .rowStyle {
   background: #fff9c4;
   border-radius: 10px;
-  padding: 2px 5px;
+  padding: 5px;
   display: inline-block;
 }
 </style>
